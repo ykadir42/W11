0a67d884d796d6eaa800b4a0eab7b3facf49dab0
bleung329 <bleung@stuy.edu>
2017-11-12 18:25:11 -0500

    cleaned up

--- main.c
+++ main.c
@@ -2,13 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#define N      20  // For this example, we will allocate 20 strings
-#define LENGTH 10
 
 char** parse_args(char* yeah)
 {
 	char * yeahcpy = (char *)malloc(sizeof(yeah));
 	strcpy(yeahcpy,yeah);
+
 	char **arr = (char**) malloc(sizeof *arr * N); 
 	if (arr)
 	{
@@ -22,54 +21,17 @@ char** parse_args(char* yeah)
 	    i++;
 	  }
 	}
+	free(yeahcpy);
 	return arr;
 
 }
 
-/*
-char ** parse_args(char* sting)
-{
-	char* temp = sting;
-	char * ans[512];
-	int i = 0;
-	for (int j = 0; j<511; j++)
-	{
-		ans[j] = malloc(32);
-	}	
-	printf("made\n");
-	while((temp = strsep(&temp," ")) != NULL)
-	{
-		printf("made!\n");
-		printf("%s\n",temp);
-	}
-	return ans;
-}
-	ans[i] = NULL;
-	return ans;
-char ** parse_args( char * line ){
-  char ** ans = (char **)malloc((sizeof(line)+1));
-  int i = 0;
-  while(line){
-    ans[i] = strsep(&line, " ");
-    i++;
-  }
-  ans[i] = NULL;
-  return ans;
-}
-*/
 int main(){
-  //char line[100] = "wow-this-is-cool";
-  //char *s1 = line;
-  //printf("[%s]\n", strsep( &s1, "-" ));
-  //printf("[%s]\n", s1);
 	char* strung = "ls -al .";
   char** cmd = parse_args(strung);
   printf("\nMade it, executing command now.\n");
   execvp(cmd[0],cmd);
-  /*while(args[i])
-    printf("%s\n", args[i]);
-  free(args);*/
-  //return 0;
+  free(cmd);
   return 0;
 
 }
