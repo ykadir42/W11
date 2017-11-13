965c9e6f987ef147f60225c4b2c5b3cacb9c3b57
bleung329 <bleung@stuy.edu>
2017-11-13 07:54:40 -0500

    comments

--- main.c
+++ main.c
@@ -3,6 +3,8 @@ Jawad Kadir
 Brian Leung
 HW 11
 */
+
+//INCLUSIONS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,33 +12,39 @@ HW 11
 
 char** parse_args(char* yeah)
 {
+	//Init copy of argstring and array to store it.
 	char * yeahcpy = (char *)malloc(sizeof(yeah));
 	strcpy(yeahcpy,yeah);
-	char **arr = (char**) malloc(sizeof *arr * 128); 
+	char **arr = (char**) malloc(sizeof *arr * 8); 
+
 	if (arr)
 	{
+	  //The i is just to help me keep track of the strings.
 	  int i = 0;
 	  while(yeahcpy!=NULL)
 	  {
-	    arr[i] = malloc(sizeof *arr[i] * 128);
-	    printf("yeahcpy is %s\n",yeahcpy );
+	  	//Copy the strsepped string into the array.
+	    arr[i] = malloc(sizeof *arr[i] * 8);
+	    printf("yeahcpy is %s\n",yeahcpy);
 	    strcpy(arr[i], strsep(&yeahcpy," ")); 
-	    printf("arr[%d] is %s\n",i,arr[i] );
+	    printf("arr[%d] is %s\n",i,arr[i]);
 	    i++;
 	  }
 	}
+	//Free the copied string.
 	free(yeahcpy);
 	return arr;
 
 }
 
 int main(){
-	char* strung = "ls -al .";
+  //<strung> is the test string.
+  char* strung = "ls -al .";
   char** cmd = parse_args(strung);
   printf("\nMade it, executing command now.\n");
   execvp(cmd[0],cmd);
+  
   free(cmd);
   return 0;
-
 }
 
